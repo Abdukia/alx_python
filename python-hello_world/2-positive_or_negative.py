8b54c95293bbbd29f3d7401de0ea3259c34af29f
Abdukia <contactalkader@gmail.com>
2023-09-13 02:28:20 -0700

    Positive anything is better than negative nothing

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,7 @@
+import random
+positive = random.randint(1, 10)
+zero = random.randint(0, -0)
+negative = random.randint(-10, 0)
+
+
+print(" {} is positive \n {} is zero \n {} is negative \n" .format(positive, zero ,negative))
