8db8dd3b9120f5b23edba4bb225358ab6197a286
Abdukia <contactalkader@gmail.com>
2023-09-13 01:16:20 -0700

    Copy - Cut - Paste

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,6 @@
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 word = "Holberton"
-print("First 3 letters: " + word[0:-6])
-word = "Holberton"
-print("Last 2 letters: " + word[7:])
-word = "Holberton"
-print("Middle word: " + word[1:-1])
+
+print("First 3 letters: {}" .format (word[0:-6]))
+print("Last 2 letters:{} " .format (word[7:]))
+print("Middle word:{} "  .format (word[1:-1]))
